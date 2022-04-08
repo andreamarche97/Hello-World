a582781da36ec417fbaf0558d1d70d76401466f7
Andrea <andreamarcheggiani18@gmail.com>
2022-04-08 17:22:14 +0200

    commit 2 update

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
-		System.out.println("upload second commit!");
+		System.out.println("upload second commit! update");
 
 	}
 
